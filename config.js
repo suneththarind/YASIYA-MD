bb80b0287a3ca60ce305dc6dcdd5e2cf9c7ebc9c
suneththarind <128328188+suneththarind@users.noreply.github.com>
2025-10-08 17:17:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?qu6iF5'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?pCuvRF'
 };
